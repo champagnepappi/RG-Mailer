f907866affa843855e58fc39de4417f02b0a8eba
Kevin Santos <kevinochieng548@gmail.com>
2019-05-09 16:27:06 +0300

    define the welcome email method in user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,7 @@
 class UserMailer < ApplicationMailer
+  def welcome_email
+    @user = params[:user]
+    @url = 'localhost:3000'
+    mail(to: @user.email, subject: 'Welcome to RGMailer')
+  end
 end
