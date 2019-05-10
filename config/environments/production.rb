8d31bdc32daec26775cb762e6d93e998c0210c03
Kevin Santos <kevinochieng548@gmail.com>
2019-05-10 23:19:35 +0300

    use sendgrid for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,14 +34,14 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
   host = "rgmailer.herokuapp.com"
   config.action_mailer.default_url_options = { host: host }
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "herokuapp.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
+    :enable_starttls_auto => true
   }
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
