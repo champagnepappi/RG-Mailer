b2efc7e2d67b3b58b9dcf07a3c2268271266aea1
Kevin Santos <kevinochieng548@gmail.com>
2019-05-10 21:59:23 +0300

    Add email attachments

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,7 @@ class UserMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
     @url = 'localhost:3000'
+    attachments.inline['c.png'] = File.read("#{Rails.root}/app/assets/images/c.png")
     mail(to: @user.email, subject: 'Welcome to RGMailer')
   end
 end
